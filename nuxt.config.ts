934d2b3087c595fd630b59c0debb4bc1c630841f
nesb01t <nesb01t@163.com>
2024-03-29 23:00:16 +0800

    update: fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-
-    typescript: {
-        typeCheck: true
-    },
-
-    imports: {
-        presets: [
-            {
-                from: 'vue-i18n',
-                imports: ['useI18n']
-            }
-        ]
-    },
-
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt',
-        '@nuxtjs/eslint-module',
-        '@nuxtjs/i18n',
-        '@formkit/auto-animate',
-        '@vueuse/nuxt',
-        '@nuxtjs/color-mode',
-        'nuxt-icon',
-    ]
-})
+  ssr: false,
+  devtools: { enabled: false },
+  imports: {
+    presets: [
+      {
+        from: "vue-i18n",
+        imports: ["useI18n"],
+      },
+    ],
+  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/i18n",
+    "@formkit/auto-animate",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "nuxt-icon",
+  ],
+});
